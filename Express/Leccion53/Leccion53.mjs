6045abb6c0ca8c4f48a45e9e95f99b4409a371b8
Carloxxz <carloxmz@gmail.com>
2024-01-20 23:24:17 -0600

    Practicas con routes

--- Express/Leccion53/Leccion53.mjs
+++ Express/Leccion53/Leccion53.mjs
@@ -7,5 +7,29 @@ app.listen(port, () => {
 });
 
 app.get("/", (req, res) => {
-  res.send("Hello world!")
+  res.send("<h1>Hello world!</h1>")
+})
+
+app.get("/about", (req, res) => {
+  res.send("<h1>Sobre mi</h1><p>My nombre es Carlos</p>")
+})
+
+app.get("/contact", (req, res) => {
+  res.send("<h1>Contactame</h1><p>Phone: +2347438834</p>")
+})
+
+app.post("/register", (req, res) => {
+  res.sendStatus(201)
+})
+
+app.put("/user/carlos", (req, res) => {
+  res.sendStatus(200)
+})
+
+app.patch("/user/carlos", (req, res) => {
+  res.sendStatus(200)
+})
+
+app.delete("/user/carlos", (req, res) => {
+  res.sendStatus(200)
 })

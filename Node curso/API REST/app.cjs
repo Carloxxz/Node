dcff1a03bc9e7242a2c2dd223140dc1a4623524c
Carloxxz <carloxmz@gmail.com>
2024-03-01 01:35:49 -0600

    POST agregado sin validar valores

--- Node curso/API REST/app.cjs
+++ Node curso/API REST/app.cjs
@@ -1,10 +1,13 @@
 const express = require('express')
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
 
 const app = express()
 const PORT = process.env.PORT ?? 1234
+app.use(express.json())
 
 app.disable('x-powered-by')
+
 /*
 app.get('/', (req, res) => {
 
@@ -18,17 +21,53 @@ app.get('/', (req, res) => {
 */
 
 app.get('/movies', (req, res) => {
+    const { genre } = req.query
+    if (genre) {
+        const filterMovies = movies.filter(
+            movie => movie.genre.some(g => g.toLowerCase() == genre.toLowerCase())
+        )
+        return res.json(filterMovies)
+    }
     res.json(movies)
 })
 
 app.get('/movies/:id', (req, res) => {
     const { id } = req.params
     const movie = movies.find(movie => movie.id == id)
-    if(movie) return res.json(movie)
+    if (movie) return res.json(movie)
 
     res.status(404).json({ message: 'Movie not found' })
 })
 
+app.post('/movies', (req, res) => {
+    const {
+        title,
+        genre,
+        year,
+        director,
+        duration,
+        rate,
+        poster
+    } = req.body
+
+    //No es REST porque se guarda el estado
+    //de la aplicación en memoria
+
+    const newMovie = {
+        id: crypto.randomUUID(), // uuid v4 universal unique id
+        title,
+        genre,
+        year,
+        director,
+        duration,
+        rate: rate ?? 0,
+        poster
+    }
+
+    movies.push(newMovie)
+    res.status(201).json(newMovie) //actualizar la caché del cliente
+})
+
 app.listen(PORT, () => {
-     console.log(`Servidor escuchando en http://localhost:${PORT}`)
+    console.log(`Servidor escuchando en http://localhost:${PORT}`)
 })
